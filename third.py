9d288dd55cd5914ae00b3aec87fdce74f8063976
AdamKhagar <adamhagarov@gmail.com>
2022-07-26 20:46:28 +0300

    done

--- third.py
+++ third.py
@@ -1,68 +1,35 @@
-from typing import Dict, List, Tuple
-
+from typing import Dict, List, Set
 
 class Interval:
-    def __init__(self, intervals: List):
-        assert len(intervals) % 2 == 0, "array length shoud be even number"
-        self.lst = []
-
-        counter = 0
-        while counter < len(intervals):
-            self.lst.append((intervals[counter], intervals[counter + 1]))
-            counter += 2
-
-        self.lst = tuple(self.lst)
-        self.idx: int = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self) -> Tuple[int]:
-        try:
-            item = self.lst[self.idx]
-        except IndexError:
-            raise StopIteration()
-        
-        self.idx += 1
-        return item
-
-    def __getitem__(self, index) -> Tuple[int]:
-        return self.lst[index]
+    def __init__(self, start: int, end: int):
+        self.start: int = start
+        self.end: int = end
 
     @property
-    def start(self) -> int:
-        return self.lst[0]
+    def range(self):
+        return range(self.start, self.end + 1)
 
-    @property
-    def end(self) -> int:
-        return self.lst[-1]
 
+def get_interval_ranges(array: List[int]) -> Set[int]:
+    i = 0
+    ranges = set()
+    while i < len(array):
+        ranges.update(range(array[i], array[i + 1]))
+        i += 2
+
+    return ranges
 
 def appearance(intervals: Dict) -> int:
-    lesson = Interval(intervals["lesson"])
-    tutor = Interval(intervals["tutor"])
-    pupil = Interval(intervals["pupil"])
+    lesson = get_interval_ranges(intervals["lesson"])
+    tutor = get_interval_ranges(intervals["tutor"])
+    pupil = get_interval_ranges(intervals["pupil"])
 
-    start_position = lesson.start
-    if start_position < tutor.start:
-        start_position = tutor.start
-    if start_position < pupil.start:
-        start_position = pupil.start
+    common_range = lesson.intersection(tutor)
+    common_range = common_range.intersection(pupil)
 
-    end_position = lesson.start
-    if end_position > tutor.start:
-        end_position = tutor.start
-    if end_position > pupil.start:
-        end_position = pupil.start
+    return len(common_range)
 
-    common_time = 0
 
-    pupil_i = next(pupil)
-    tutor_i = next(tutor)
-
-    for i in range(start_position, end_position + 1):
-        if (pupil_i[0] <= i and i <= pupil_i[1]) and (tutor_i[0] <= i and i <= tutor_i[1]):
-            common_time += 1
 
 tests = [
     {
@@ -91,6 +58,7 @@ tests = [
 ]
 
 if __name__ == '__main__':
-   for i, test in enumerate(tests):
-       test_answer = appearance(test['data'])
-       assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+    for i, test in enumerate(tests):
+        test_answer = appearance(test['data'])
+        assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+        print(f"#{i}", test_answer, "Passed") 
